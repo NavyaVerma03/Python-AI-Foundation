7a0aaa36d3ed76dee8568b52da51ef07f256070a
Navya Verma <navyaverma451@gmail.com>
2026-06-27 22:29:06 +0530

    Initial Commit

---
+++ Practice Question/ArmstrongNumber.py
@@ -0,0 +1,16 @@
+# Check whether a number is an Armstrong number.
+num = int(input("Enter a number: "))
+
+temp = num
+digits = len(str(num))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + digit ** digits
+    temp = temp // 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
